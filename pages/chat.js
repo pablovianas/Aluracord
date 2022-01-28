7558a41c825a8e2a6ae81c8e2f0f7393f7267a95
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-01-28 12:16:08 -0300

    Lesson 4, integration with supabase.io

--- pages/chat.js
+++ pages/chat.js
@@ -1,35 +1,54 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM3ODA5NywiZXhwIjoxOTU4OTU0MDk3fQ.OZyBU2zhYAL6ZcXWYD3lLVTxKS2vQO8WHnO731jomDk'
+const SUPABASE_URL = 'https://dwghgzffnsulnbnxtogo.supabase.co'
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+
 
 export default function ChatPage() {
     const [mensagem, setMensagem] = React.useState('');
     const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
-    /*
-    // Usuário
-    - Usuário digita no campo textarea
-    - Aperta enter para enviar
-    - Tem que adicionar o texto na listagem
-    
-    // Dev
-    - [X] Campo criado
-    - [X] Vamos usar o onChange usa o useState (ter if pra caso seja enter pra limpar a variavel)
-    - [X] Lista de mensagens 
-    */
+    React.useEffect(() => {
+        supabase
+            .from('mensagens')
+            .select('*')
+            .order('id', { ascending: false })
+            .then((dados) => {
+                setListaDeMensagens(dados.data);
+
+            });
+    }, []);
+
     function handleNovaMensagem(novaMensagem) {
         const mensagem = {
-            id: listaDeMensagens.length + 1,
             de: 'pablovianas',
             texto: novaMensagem,
         };
 
-        setListaDeMensagens([
-            mensagem,
-            ...listaDeMensagens,
-        ]);
+        supabase
+            .from('mensagens')
+            .insert([
+                mensagem
+            ])
+            .then(({ data }) => {
+                setListaDeMensagens([
+                    data[0],
+                    ...listaDeMensagens,
+                ]);
+            })
+
+
         setMensagem('');
     }
+
+
+
 
     return (
         <Box
@@ -71,13 +90,7 @@ export default function ChatPage() {
                     }}
                 >
                     <MessageList mensagens={listaDeMensagens} />
-                    {/* {listaDeMensagens.map((mensagemAtual) => {
-                        return (
-                            <li key={mensagemAtual.id}>
-                                {mensagemAtual.de}: {mensagemAtual.texto}
-                            </li>
-                        )
-                    })} */}
+
                     <Box
                         as="form"
                         styleSheet={{
@@ -153,7 +166,7 @@ function Header() {
 }
 
 function MessageList(props) {
-    console.log(props);
+
     return (
         <Box
             tag="ul"
@@ -180,6 +193,7 @@ function MessageList(props) {
                                 backgroundColor: appConfig.theme.colors.neutrals['transparent'],
                             }
                         }}
+
                     >
                         <Box
                             styleSheet={{
@@ -194,7 +208,7 @@ function MessageList(props) {
                                     display: 'inline-block',
                                     marginRight: '8px',
                                 }}
-                                src={`https://github.com/pablovianas.png`}
+                                src={`https://github.com/${mensagem.de}.png`}
                             />
                             <Text tag="strong">
                                 {mensagem.de}
@@ -210,6 +224,22 @@ function MessageList(props) {
                             >
                                 {(new Date().toLocaleDateString())}
                             </Text>
+
+                            <Button
+
+                                colorVariant="negative"
+                                label="Delete"
+                                variant="secondary"
+                                styleSheet={{
+                                    marginLeft: '20px',
+
+                                }}
+
+                            />
+
+
+
+
                         </Box>
                         {mensagem.texto}
                     </Text>
